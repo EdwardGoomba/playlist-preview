76d0a234859c65535d22032130b592bdd54c0d5d
EdwardGoomba <ask@edwardvdesign.com>
2018-01-05 00:09:45 -0500

    Create initial files

---
+++ app.js
@@ -0,0 +1,41 @@
+
+
+function Playlist() {
+
+}
+
+Playlist.prototype.add = function() {
+
+};
+
+Playlist.prototype.play = function() {
+
+};
+
+Playlist.prototype.stop = function(){
+
+};
+
+Playlist.prototype.next = function() {
+
+};
+
+Playlist.prototype.renderInElement = function() {
+
+};
+
+function Song() {
+
+}
+
+Song.prototype.play = function() {
+
+};
+
+Song.prototype.stop = function() {
+
+};
+
+Song.prototype.toHTML = function() {
+
+};
