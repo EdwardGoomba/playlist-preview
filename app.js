a9965949af3c9034f477ee41d7fc2a6e78c7262a
EdwardGoomba <ask@edwardvdesign.com>
2018-01-05 10:18:52 -0500

    Separate objects, finish player, debug

--- app.js
+++ app.js
@@ -1,103 +1,43 @@
-// Playlist
-
-function Playlist() {
-  this.songs = [];
-  this.nowPlayingIndex = 0;
-}
-
-Playlist.prototype.add = function(song) {
-  this.songs.push(song);
-};
-
-Playlist.prototype.play = function() {
-  var currentSong = this.song[this.nowPlayingIndex];
-  currentSong.play();
-};
-
-Playlist.prototype.stop = function(){
-  var currentSong = this.song[this.nowPlayingIndex];
-  currentSong.stop();
-};
-
-Playlist.prototype.next = function() {
-  this.stop();
-  this.nowPlayingIndex++;
-  if(this.nowPlayingIndex === this.songs.length) {
-    this.nowPlayingIndex = 0;
-  }
-
-  this.play();
-};
-
-
-Playlist.prototype.renderInElement = function(list) {
-  list.innerHTML = '';
-
-  for(var i = 0; i < this.songs.length; i++) {
-    list.innerHTML += this.songs[i].toHTML();
-  }
-};
-
-// Song
-
-function Song(title, artist, duration) {
-  this.title = title;
-  this.artist = artist;
-  this.duration = duration;
-  this.isPlaying = false;
-}
-
-Song.prototype.play = function() {
-  this.isPlaying = true;
-};
-
-Song.prototype.stop = function() {
-  this.isPlaying = false;
-};
-
-Song.prototype.toHTML = function() {
-  var htmlString = '<li>';
-
-  if(this.isPlaying) {
-    htmlString += ' class="current">';
-  }
-
-  htmlString += this.title;
-  htmlString += ' - ';
-  htmlString += this.artist;
-  htmlString += '<span class="duration">';
-  htmlString += this.duration;
-  htmlString += '</span></li>';
-  return htmlString;
-
-};
-
 var playlist = new Playlist();
-var hereComesTheSun = new Song('Here Comes the Sun', 'The Beatles', '2:54');
 
-var walkingOnSunshiine = new Song('Walking on Sunshine', 'Katrina and the Waves', '3:43');
+var hereComesTheSun = new Song("Here Comes the Sun", "The Beatles", "2:54");
+var walkingOnSunshine = new Song("Walking on Sunshine", "Katrina and the Waves", "3:43");
+
+var manOfSteel = new Movie("Man of Steel", 2013, "2:23:00");
 
 playlist.add(hereComesTheSun);
-playlist.add(walkingOnSunshiine);
+playlist.add(walkingOnSunshine);
 
-var playlistElement = document.getElementById('playlist');
+playlist.add(manOfSteel);
+
+var playlistElement = document.getElementById("playlist");
 
 playlist.renderInElement(playlistElement);
 
-var playButton = document.getElementById('play');
+var playButton = document.getElementById("play");
 playButton.onclick = function() {
   playlist.play();
   playlist.renderInElement(playlistElement);
 }
 
-var nextButton = document.getElementById('next');
+var nextButton = document.getElementById("next");
 nextButton.onclick = function() {
   playlist.next();
   playlist.renderInElement(playlistElement);
 }
 
-var stopButton = document.getElementById('stop');
+var stopButton = document.getElementById("stop");
 stopButton.onclick = function() {
   playlist.stop();
   playlist.renderInElement(playlistElement);
 }
+
+
+
+
+
+
+
+
+
+
